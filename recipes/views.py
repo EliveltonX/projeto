9476fde9d07d110383633323714558387ed943e0
EliveltonX <eliveltonalmeidapardini@gmail.com>
2022-07-19 23:46:52 -0400

    primeira font awesome do projeto

--- recipes/views.py
+++ recipes/views.py
@@ -5,9 +5,3 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request,'recipes/home.html')
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-def contato(request):
-    return HttpResponse('contato')
