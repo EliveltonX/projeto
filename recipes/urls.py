b97dd4c5ed77f3e9bfa64b71c2660bbe78f19c6d
EliveltonX <eliveltonalmeidapardini@gmail.com>
2022-07-26 01:26:18 -0400

    Criando migration e migrando e models
    foreign key nos models
    
    django admin
    
    logica nas urls das categorias
    
    Crud na area de admin e no shell do django
    
    criando receitas na DB
    
    Salvando img com pillow
    criando Media_url e Media_root do projeto

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,5 +6,6 @@ app_name = 'myRecipes'
 
 urlpatterns = [
     path('',views.home, name="home"), #Home
+    path('recipes/category/<int:category_id>/',views.category,name="category"), #recipes
     path('recipes/<int:id>/',views.recipes,name="recipe"), #recipes
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from utils.recipes.recipe_factory import *
+from recipes.models import Recipe
+from django.http import Http404
 
 # Create your views here.
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published = True).order_by('-id')
     return render(request,'recipes/pages/home.html', context={
-        'recipes':[make_recipe() for _ in range(10)],
+        'recipes':recipes,
     })
+
+def category(request,category_id):
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not Dound :(')
+    else:
+        return render(request,'recipes/pages/category.html', context={
+            'recipes':recipes,
+        })
+
+
+
 
 def recipes(request,id):
     return render(request,'recipes/pages/recipe-view.html', context={
