677e365999ea970f8ef5ced76114896f4da800f0
EliveltonX <eliveltonalmeidapardini@gmail.com>
2022-08-29 19:03:03 -0400

    criando login e logout
    criando segurança csfr token
    
    criando botão de logout
    
    craindo urls de login e logout

--- authors/views.py
+++ authors/views.py
@@ -1,4 +1,6 @@
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
@@ -32,6 +34,7 @@ def register_create(request):
         messages.success(request, 'Seu usuario foi criado')
 
         del(request.session['register_form_data'])
+        return redirect(reverse('authors:login'))
 
     return redirect('authors:register')
 
@@ -45,4 +48,35 @@ def login_view(request):
 
 
 def login_create(request):
-    return render(request, 'authors/pages/login.html')
+    if not request.POST:
+        raise Http404()
+    form = LoginForm(request.POST)
+    login_url = reverse('authors:login')
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+        if authenticated_user is not None:
+            messages.success(request, 'Você esta logado.')
+            login(request, authenticated_user)
+            return redirect(login_url)
+
+        messages.error(request, 'Invalid credentials')
+        return redirect(login_url)
+
+    messages.error(request, 'Error to validate your credentials')
+    return redirect(login_url)
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def logout_view(request):
+    if not request.POST:
+        return redirect(reverse('authors:login'))
+
+    if request.POST.get('username') != request.user.username:
+        return redirect(reverse('authors:login'))
+
+    logout(request)
+    return redirect(reverse('authors:login'))
